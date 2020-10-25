3aa2b1f3ad9d1e0e5d9e4a1efe198b2acc750a72
Ahmed <ahmad.g.mustafa@gmail.com>
2020-10-25 02:43:04 +0200

    fix safari flex bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 // tailwind.config.js
 module.exports = {
   theme: {
-    screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
-    },
+    // screens: {
+    //   sm: '640px',
+    //   md: '768px',
+    //   lg: '1024px',
+    //   xl: '1280px',
+    // },
     fontFamily: {
       display: ['Gilroy', 'sans-serif'],
       body: ['Graphik', 'sans-serif'],
